bbdadbe8b94fdaf860ec78789510092a36fd1b90
guilfb <guillaumelefebvre69@gmail.com>
2025-02-19 09:34:35 +0100

    Added POST route

--- lrs-connector/src/main.py
+++ lrs-connector/src/main.py
@@ -1,8 +1,10 @@
 from logging import basicConfig, getLogger, INFO
-from fastapi import FastAPI, HTTPException, Query, Request
+from typing import Any, Dict, List
+
+from fastapi import Depends, FastAPI, HTTPException, Query, Request
 from fastapi.responses import JSONResponse
-from pydantic import AnyHttpUrl
-from requests import get as requests_get
+from pydantic import AnyHttpUrl, BaseModel
+from requests import get as requests_get, post as requests_post
 from urllib.parse import urlparse, urlencode
 
 basicConfig(level=INFO)
@@ -11,6 +13,12 @@ logger = getLogger(__name__)
 XAPI_ENDPOINT = 'xapi_endpoint'
 XAPI_VERSION = 'xapi_version'
 
+
+class InputQueryModel(BaseModel):
+    xapi_endpoint: AnyHttpUrl = Query(min_length=1)
+    xapi_version: str = Query(min_length=1)
+
+
 app = FastAPI(
     title="LRS Connector",
     description="LRS Connector is a lightweight FastAPI application designed to act as a proxy for querying Learning Record Stores (LRS) such as Learning Locker or Ralph, from the DataSpace.",
@@ -18,16 +26,18 @@ app = FastAPI(
 )
 
 @app.get("/statements", tags=["statements"])
-async def get_statements(request: Request, xapi_endpoint: AnyHttpUrl = Query(min_length=1), xapi_version: str = Query(min_length=1)):
-    # Extract headers and query parameters
+async def get_statements(request: Request, query: InputQueryModel = Depends()):
+    # Extract headers
     headers = {key: value for key, value in request.headers.items() if key.lower() != 'host'}
-    headers["X-Experience-API-Version"] = xapi_version
+    headers["X-Experience-API-Version"] = query.xapi_version
+    
+    # Extract query parameters
     query_params = {key: value for key, value in request.query_params.items() if key not in [XAPI_ENDPOINT, XAPI_VERSION]}
     query_string = urlencode(query_params)
 
     try:
         # Parse the xAPI endpoint
-        parsed_url = urlparse(str(xapi_endpoint))
+        parsed_url = urlparse(str(query.xapi_endpoint))
 
         # Extract the base URL and the base path
         lrs_base_url = parsed_url.scheme + "://" + parsed_url.netloc
@@ -35,7 +45,6 @@ async def get_statements(request: Request, xapi_endpoint: AnyHttpUrl = Query(min
 
         # Build the URL dynamically
         api_url = f"{lrs_base_url}{xapi_base_path}" + (f"?{query_string}" if query_string else "")
-        logger.debug("Built the URL: %s", api_url)
     except Exception as e:
         logger.error("Couldn't build the URL: %s", e)
         raise HTTPException(status_code=500, detail="Couldn't build the URL") from e
@@ -62,3 +71,19 @@ async def get_statements(request: Request, xapi_endpoint: AnyHttpUrl = Query(min
             raise HTTPException(status_code=500, detail="Couldn't parse the response from the LRS") from e
 
     return JSONResponse(accumulated_statements)
+
+@app.post("/statements", tags=["statements"])
+async def post_statement(request: Request, payload: List[Dict[Any, Any]], query: InputQueryModel = Depends()):
+    # Extract headers
+    headers = {key: value for key, value in request.headers.items() if key.lower() != 'host'}
+    headers["X-Experience-API-Version"] = query.xapi_version
+
+    # Send the POST request to the LRS
+    try:
+        response = requests_post(url=query.xapi_endpoint, headers=headers, json=payload)
+        response.raise_for_status()
+    except Exception as e:
+        logger.error("Couldn't post data to the LRS: %s", e)
+        raise HTTPException(status_code=500, detail="Couldn't post data to the LRS") from e
+
+    return JSONResponse(content=response.json())
